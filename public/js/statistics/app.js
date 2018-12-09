1335224b7637e2c6dd34ce4614f579a99193dea8
Kim Sun Wook <sunus7@naver.com>
2018-12-09 17:14:43 +0900

    5:14

--- public/js/statistics/app.js
+++ public/js/statistics/app.js
@@ -6,6 +6,43 @@ angular.module('workingHoursTrello', [
 
 }).run(function(){
 
+
+
+  // Variable Section
+  $rootScope.monent = moment();
+
+
+
+  // Increase Function Section
+  $rootScope.increaseWeek = function(){
+    $rootScope.monent.add(7, 'days');
+  };
+
+  $rootScope.increaseMonth = function(){
+    $rootScope.monent.add(1, 'months');
+  };
+
+  $rootScope.increaseYear = function(){
+    $rootScope.monent.add(1, 'years');
+  };
+
+
+
+  // Decrease Function Section
+  $rootScope.decreaseWeek = function(){
+    $rootScope.monent.subtract(7, 'days');
+  };
+
+  $rootScope.decreaseMonth = function(){
+    $rootScope.monent.subtract(1, 'months');
+  };
+
+  $rootScope.decreaseYear = function(){
+    $rootScope.monent.subtract(1, 'years');
+  };
+
+
+
   var t = window.TrelloPowerUp.iframe();
   var months;
   t.render(function(){
@@ -19,5 +56,41 @@ angular.module('workingHoursTrello', [
       }
     });
   });
+
+  // Examples
+  var years = [
+    {
+      year:2018,
+      // Months
+      months:[
+        {
+          month: 1,
+          // Weeks
+          weeks: [
+            {
+              week: 1,
+              // Days
+              days: [
+                {
+                  day:1
+                  // Users
+                  users:[
+                    {
+                      name:'Kim Sun Wook',
+                      time_text:'10-19.5',
+                      time_number:9.5,
+                      is_annual:false,
+                      is_national:false,
+                      is_birthday:false
+                    }
+                  ]
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    }
+  ];
 
 });
