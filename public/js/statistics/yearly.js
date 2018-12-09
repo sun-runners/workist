97def0e74c73e50d0a29956766f921bf43a8f8e6
Kim Sun Wook <sunus7@naver.com>
2018-12-09 15:33:39 +0900

    3:33

--- public/js/statistics/yearly.js
+++ public/js/statistics/yearly.js
@@ -3,9 +3,22 @@ var t = TrelloPowerUp.iframe();
 t.render(function(){
 });
 
+// var t = window.TrelloPowerUp.iframe();
+// var months;
+// t.render(function(){
+//   t.lists('all')
+//   .then(function (lists) {
+//     months = lists;
+//     for(var i=0; i<lists.length; i++){
+//       var list = lists[i];
+//       if(!list.name) continue;
+//     }
+//   });
+// });
+
 var app = new Vue({
   el: '#app',
   data: {
-    months: months
+    years: '12312312'
   }
 });
