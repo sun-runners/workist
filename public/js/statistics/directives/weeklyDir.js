936d837c26444438fd3b907e2f21b2e3232cb6fd
Evan <eevan4256@gmail.com>
2019-01-15 16:56:48 +0800

    remove controller from directive() and added $http

--- public/js/statistics/directives/weeklyDir.js
+++ public/js/statistics/directives/weeklyDir.js
@@ -1,13 +1,13 @@
 'use strict';
 
 angular.module('workingHoursTrello')
-	.directive('weeklyDir', function ($rootScope) {
+	.directive('weeklyDir',  function ($rootScope, $http) {
 		return {
 			link : function(scope, element, attrs){
 
 				// Initialize Function Section
 				var initialize = function(){
-          var moment_original = $rootScope.moment.clone();
+          		var moment_original = $rootScope.moment.clone();
 					var dt_original = $rootScope.getDtOfMoment(moment_original);
 					scope.dts = [];
 					for(var i=1; i<=7; i++){
@@ -24,22 +24,23 @@ angular.module('workingHoursTrello')
 						// Push dt
 						scope.dts.push(dt_in_week);
 					}
+				
 				};
 
 				// Watch Function Section
-				$rootScope.$watch('moment', function(){
+				$rootScope.$watch('moment', function( $scope, servTrelloApiGet){
+	
 					initialize();
 			  }, true);
 
+				$http.get('https://api.trello.com/1/boards/5ba38efef50b8979566922d0/members?key=86b2621fa79c88d61ff3a95b82ec2bd7&token=7be1976d0063e2ca94d145fbf01604667dfee015cfe1b4cd41a355d76a1ca118')
+				.then(function (response) {
+					scope.boardMembers = response.data;
+				});
 			},
 			restrict: "EA",
 			replace: true,
 			scope: {},
-			templateUrl: "../../template/statistics/directives/weeklyDir.html",
-			controller: function($scope, servTrelloApiGet) {
-				servTrelloApiGet.getBoardMembers().then(function(response) {
-					$scope.boardMembers = response.data;
-				});
-			}
+			templateUrl: "../../template/statistics/directives/weeklyDir.html"
 		}
 	});
