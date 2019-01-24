3cf11f962c0f0ec425d553c108a2343b83e479b5
Evan <eevan4256@gmail.com>
2019-01-23 16:55:53 -1100

    Added Calculate functions for cards hours, specific Lists etc

--- public/js/statistics/directives/weeklyDir.js
+++ public/js/statistics/directives/weeklyDir.js
@@ -1,7 +1,5 @@
-'use strict';
-
 angular.module('workingHoursTrello')
-	.directive('weeklyDir',  function ($rootScope, $http) {
+	.directive('weeklyDir',  function ($rootScope, serviceGetApi) {
 		return {
 			link : function(scope, element, attrs){
 
@@ -24,23 +22,87 @@ angular.module('workingHoursTrello')
 						// Push dt
 						scope.dts.push(dt_in_week);
 					}
-				
 				};
-
 				// Watch Function Section
-				$rootScope.$watch('moment', function( $scope, servTrelloApiGet){
-	
+				$rootScope.$watch('moment', function( $scope){
 					initialize();
 			  }, true);
 
-				$http.get('https://api.trello.com/1/boards/5ba38efef50b8979566922d0/members?key=86b2621fa79c88d61ff3a95b82ec2bd7&token=7be1976d0063e2ca94d145fbf01604667dfee015cfe1b4cd41a355d76a1ca118')
-				.then(function (response) {
-					scope.boardMembers = response.data;
+//   ---------------------------------------------------- fetching API Getting Names & Cards ------------------------------		   
+				// Get Boards Members
+				serviceGetApi.getBoardMembers().then(function(response) {
+					scope.boardMembers = response.data; 
 				});
+				// Get Boards Lists
+				serviceGetApi.getBoardLists().then(function(response){
+					scope.boardLists = response.data;
+				});
+				// Get Boards Cards
+				serviceGetApi.getBoardCards().then(function(response){
+					scope.boardCards = response.data;
+				});
+
+				// we Find the Card to show Base on the Owners ID and Lists ID
+				scope.findCard = function(ownerId, cardId){
+					let foundCard = scope.boardCards.find(function(card, index){
+						return card.idMembers == ownerId && card.idList == cardId;
+					});
+					return foundCard;	
+				}
+				scope.findBoardList = function(dateWanted){
+					// parse Date into the same Format as Lists Name
+					getDate = dateWanted.getFullYear() + '/' + ('0' + (dateWanted.getMonth() + 1)).slice(-2) + '/' + dateWanted.getDate()
+
+				    let foundList = scope.boardLists.find(function(list, index){
+				      	let listNameBeforeSpace = list.name.substr(0,list.name.indexOf(' '));
+				      	return listNameBeforeSpace == getDate;
+				    }); 
+				    // will return the ID of the Given Date
+			  		return foundList.id;
+			  	}
+			  		// We Calculate the Number of Hours from the Card's Name
+			  	scope.calculateCardNameToHours = function(card){
+			  		let totalHours = eval(card.name);
+			  		return Math.abs(totalHours);	
+			  	}
+			  	scope.calculateTheCardsHourToDay = function(cardsHour){
+			  		if (cardsHour >= 8) {
+			  			return cardsDay = 1;
+			  		}else if (cardsHour < 8 && cardsHour > 4) {
+			  			return 	cardsDay = 0.5;
+			  		}else if (cardsHour <= 4) {
+			  			return cardsDay = 0;
+			  		}
+			  	
+			  	}
+
+			  	scope.getCard = function(dateWanted, ownerId){
+			  		this.dateWanted = dateWanted;
+			  		this.ownerId = ownerId;
+		
+			  		try {
+			  			    let theList = scope.findBoardList(this.dateWanted);
+					  		// Find the Card of the Lists in the Given Day base on it's memberId.
+					  		let theCard = scope.findCard(this.ownerId, theList);
+					  		// Get the Total number of hours base on the Cards Name; ex: "11-13+21-23+23.5-24"
+					  		let totalCardHours = scope.calculateCardNameToHours(theCard);
+					  		//  Get the Day of the Card: 1, 0.5, 0. 
+					  		let CardsDay = scope.calculateTheCardsHourToDay(totalCardHours);
+					  		return CardsDay;
+			  		} catch(e) {
+			  			return 0;
+			  			console.log(e);
+			  		}
+			  		
+			  	}
+
+
+
 			},
 			restrict: "EA",
 			replace: true,
 			scope: {},
-			templateUrl: "../../template/statistics/directives/weeklyDir.html"
-		}
+			templateUrl: "../../template/statistics/directives/weeklyDir.html",
+
+	  	}
 	});
