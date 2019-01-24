08a744c9a1e4f1650d34c2646bfb7c5dfd2a58b6
Evan <eevan4256@gmail.com>
2019-01-23 16:53:28 -1100

    added Service to Fetch API

--- public/js/statistics/app.js
+++ public/js/statistics/app.js
@@ -196,3 +196,22 @@ angular.module('workingHoursTrello', [
   ];
 
 });
+angular.module('workingHoursTrello').service('serviceGetApi', function($http){
+  // get boards Members
+  this.getBoardMembers = function() {
+    return  $http.get('https://api.trello.com/1/boards/5ba38efef50b8979566922d0/members?key=86b2621fa79c88d61ff3a95b82ec2bd7&token=7be1976d0063e2ca94d145fbf01604667dfee015cfe1b4cd41a355d76a1ca118');
+  }
+
+  // get boars Lists
+  this.getBoardLists = function() {
+    return $http.get('https://api.trello.com//1/boards/5ba38efef50b8979566922d0/lists?key=86b2621fa79c88d61ff3a95b82ec2bd7&token=7be1976d0063e2ca94d145fbf01604667dfee015cfe1b4cd41a355d76a1ca118');
+  }
+
+  //get boards Cards
+  this.getBoardCards = function() {
+    return $http.get('https://api.trello.com/1/boards/5ba38efef50b8979566922d0/cards?key=86b2621fa79c88d61ff3a95b82ec2bd7&token=7be1976d0063e2ca94d145fbf01604667dfee015cfe1b4cd41a355d76a1ca118');
+  }
+});
+
+
+
