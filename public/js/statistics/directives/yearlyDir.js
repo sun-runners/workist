9880ac9376049a059f509e2c41b4417a448f0424
eevan7a9 <e7a9@protonmail.com>
2019-03-26 15:49:27 +0800

    added yearly monthly color code

--- public/js/statistics/directives/yearlyDir.js
+++ public/js/statistics/directives/yearlyDir.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('workingHoursTrello')
-	.directive('yearlyDir', function ($rootScope, apiS, monthS, yearS) {
+	.directive('yearlyDir', function ($rootScope, apiS, monthS, yearS, nationalityS, holidayS) {
 		return {
 			link : function(scope, element, attrs){
 
@@ -9,7 +9,6 @@ angular.module('workingHoursTrello')
 				var initialize = function(){
 					let moment_original = $rootScope.moment.clone();
 					scope.thisDate = moment_original;
-
 					scope.months = [
 						{month: "Jan", value: 1},{month: "Feb",value:2},
 						{month:"Mar", value: 3},{month:"Apr", value: 4},{month:"May", value: 5},{month:"Jun", value: 6},
@@ -21,12 +20,11 @@ angular.module('workingHoursTrello')
 				apiS.getBoardMembers().then((response) => scope.boardMembers = response.data /** Get Boards Members */);
 				apiS.getBoardLists().then((response) => scope.boardLists = response.data /**  Get Boards Lists */);
 				apiS.getBoardCards().then((response) => scope.boardCards = response.data /** Get Boards Cards */);
+				apiS.calendarBoardLists().then((response) => scope.calendarLists = response.data /**  Get Boards Lists */);
+				apiS.calendarBoardCards().then((response) => scope.calendarCards = response.data /** Get Boards Cards */);
 				
 				scope.getMonthlyCard = (month, memberId) => { /** To calculate the total working days of the member per month*/
 					return monthS.getMonthsValue($rootScope.dt.year, month, scope.boardLists, memberId, scope.boardCards); /** year , month, boardLists, memberId, boardCards */
-				}
-				scope.getMonthlyNeedWork = (month) => { /** Get the total days members should work per month */
-					return monthS.monthsNeedtoWork($rootScope.dt.year, month); /** year , month */
 				}
 				scope.getYearlyCard = (memberId) => { /** To calculate the total working days of the member per month*/
 					return yearS.getYearsValue($rootScope.dt.year, scope.months, memberId, scope.boardLists, scope.boardCards);	/** year , month, boardLists, memberId, boardCards */	
@@ -34,6 +32,14 @@ angular.module('workingHoursTrello')
 				scope.getYearlyNeedWork = () => { /** Get the total days members should work per year */
 					return yearS.yearsNeedToWork($rootScope.dt.year, scope.months); /** year , month */
 				}
+				scope.getToWorkTotal = (memberId, month) => {
+					let country = nationalityS.membersNationality(memberId, scope.calendarCards, scope.calendarLists);
+					let toWork = monthS.monthsNeedtoWork(scope.thisDate.year(), month); /** get the total days to work whith holiday not a factor  */
+					return holidayS.datesHoliday(country, $rootScope.dt.year, scope.calendarLists, scope.calendarCards, toWork);
+				}
+				scope.showMonthly = (work, toWork) => (work != 0) ? work+' / '+toWork : '-';
+				
+
 			},
 			restrict: "EA",
 			replace: true,
