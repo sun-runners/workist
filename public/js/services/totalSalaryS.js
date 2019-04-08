ec860f107e6b18a0b16f9581dc638462018796b7
eevan7a9 <e7a9@protonmail.com>
2019-04-08 12:17:08 +0800

    created accurate month duration

--- public/js/services/totalSalaryS.js
+++ public/js/services/totalSalaryS.js
@@ -8,20 +8,25 @@ angular.module('workingHoursTrello').service('totalSalaryS', function(){
         let foundCard = boardCards.find((card) => card.idMembers == memberId && card.idList == listId);
         return new Date(foundCard.name.substr(0,foundCard.name.indexOf(' ')));
     }
-    this.monthDiff = (d1, d2) => {
-        var months;
-        months = (d2.getFullYear() - d1.getFullYear()) * 12;
-        months -= d1.getMonth() + 1;
-        months += d2.getMonth();
-        return months <= 0 ? 0 : months;
+    this.diffInMonths = (from, to) => {
+        var months = to.getMonth() - from.getMonth() + (12 * (to.getFullYear() - from.getFullYear()));
+    
+        if(to.getDate() < from.getDate()){
+            var newFrom = new Date(to.getFullYear(),to.getMonth(),from.getDate());
+            if (to < newFrom  && to.getMonth() == newFrom.getMonth() && to.getYear() %4 != 0){
+                months--;
+            }
+        }
+    
+        return months;
     }
     
     this.monthDuration = (boardLists, boardCards, memberId, currentDate, nameToFind) => {
         try {
             let listId = this.getListByName(boardLists, nameToFind);
             let entryDate = this.getEntryDate(boardCards, listId, memberId);
-            let Month = this.monthDiff(entryDate, currentDate);
-        return  Month+1;
+            let Month = this.diffInMonths(entryDate, currentDate);
+        return  Month;
         } catch (error) {
             return '-'
         }
