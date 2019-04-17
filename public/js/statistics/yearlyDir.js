8127d5af4b20ea00c666a4bf0158286258654575
eevan7a9 <e7a9@protonmail.com>
2019-04-17 15:27:16 +0800

    refactor remove local variable of yearly Directive and Added global varialble

--- public/js/statistics/yearlyDir.js
+++ public/js/statistics/yearlyDir.js
@@ -16,31 +16,25 @@ angular.module('workingHoursTrello')
 						]									
 				};
 				initialize();
-
-				apiS.getBoardMembers().then((response) => scope.boardMembers = response.data /** Get Boards Members */);
-				apiS.getBoardLists().then((response) => scope.boardLists = response.data /**  Get Boards Lists */);
-				apiS.getBoardCards().then((response) => scope.boardCards = response.data /** Get Boards Cards */);
-				apiS.calendarBoardLists().then((response) => scope.calendarLists = response.data /**  Get Boards Lists */);
-				apiS.calendarBoardCards().then((response) => scope.calendarCards = response.data /** Get Boards Cards */);
 				
 				scope.getMonthlyCard = (month, memberId) => { /** To calculate the total working days of the member per month*/
-					return monthS.getMonthsValue($rootScope.dt.year, month, scope.boardLists, memberId, scope.boardCards); /** year , month, boardLists, memberId, boardCards */
+					return monthS.getMonthsValue($rootScope.dt.year, month, $rootScope.boardLists, memberId, $rootScope.boardCards); /** year , month, boardLists, memberId, boardCards */
 				}
 				scope.getYearlyCard = (memberId) => { /** To calculate the total working days of the member per month*/
-					return yearS.getYearsValue($rootScope.dt.year, scope.months, memberId, scope.boardLists, scope.boardCards);	/** year , month, boardLists, memberId, boardCards */	
+					return yearS.getYearsValue($rootScope.dt.year, scope.months, memberId, $rootScope.boardLists, $rootScope.boardCards);	/** year , month, boardLists, memberId, boardCards */	
 				}
 				scope.getYearlyNeedWork = () => { /** Get the total days members should work per year */
 					return yearS.yearsNeedToWork($rootScope.dt.year, scope.months); /** year , month */
 				}
 				scope.getMonthlyToWork = (memberId, month) => {
-					let country = nationalityS.membersNationality(memberId, scope.calendarCards, scope.calendarLists);
+					let country = nationalityS.membersNationality(memberId, $rootScope.calendarCards, $rootScope.calendarLists);
 					let toWork = monthS.monthsNeedtoWork(scope.thisDate.year(), month); /** get the total days to work whith holiday not a factor  */
-					return holidayS.datesWithoutHoliday(country, $rootScope.dt.year, scope.calendarLists, scope.calendarCards, toWork);
+					return holidayS.datesWithoutHoliday(country, $rootScope.dt.year, $rootScope.calendarLists, $rootScope.calendarCards, toWork);
 				}
 				scope.getYearlyToWork = (memberId) => {
-					let country = nationalityS.membersNationality(memberId, scope.calendarCards, scope.calendarLists);
+					let country = nationalityS.membersNationality(memberId, $rootScope.calendarCards, $rootScope.calendarLists);
 					let toWork = yearS.yearsNeedToWork($rootScope.dt.year, scope.months); /** get the total days to work whith holiday not a factor  */
-					return holidayS.datesWithoutHoliday(country, $rootScope.dt.year, scope.calendarLists, scope.calendarCards, toWork);
+					return holidayS.datesWithoutHoliday(country, $rootScope.dt.year, $rootScope.calendarLists, $rootScope.calendarCards, toWork);
 				}
 				scope.showMonthly = (work, toWork) => (work != 0) ? work+' / '+toWork : '-';
 
