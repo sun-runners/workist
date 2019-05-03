5562f15e1a05b0faecfecd2be5a3bdbba29c2425
eevan7a9 <e7a9@protonmail.com>
2019-05-03 15:43:54 +0800

    develop to add bonuse on thirteenth month Pay

--- public/js/salary/thirteenDir.js
+++ public/js/salary/thirteenDir.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('workingHoursTrello')
-	.directive('thirteenDir', function ($rootScope, totalSalaryS) {
+	.directive('thirteenDir', function ($rootScope, totalSalaryS, taskS, timeS, bonuseS) {
 		return {
 			link : function(scope, element, attrs){
           		function initialize() {
@@ -10,14 +10,32 @@ angular.module('workingHoursTrello')
 					scope.getMonthDuration = (memberId) => totalSalaryS.monthDuration($rootScope.calendarLists, $rootScope.calendarCards, 'ENTERING DATE', $rootScope.dt.Date, memberId);
 					// scope.getMonthDuration = (memberId) => totalSalaryS.monthDuration($rootScope.calendarLists, $rootScope.calendarCards, 'ENTERING DATE', new Date('2019/12/7'), memberId);
 					scope.getCurrentSalary = (memberId, monthNumber) => totalSalaryS.salary($rootScope.calendarLists, $rootScope.calendarCards, 'ENTERING DATE', memberId, monthNumber);
-					// scope.getCurrentSalary = (memberId, monthNumber) => totalSalaryS.salary($rootScope.calendarLists, $rootScope.calendarCards, 'ENTERING DATE', memberId, monthNumber);
 					scope.formatSalary = (salary) => {
 						try {
 							return salary.toLocaleString() + " PHP"
 						} catch (error) {}
 					}
-                    scope.totalThirteen = (salary, monthDuration) => {
-						return Math.ceil((salary * monthDuration)/12)
+					scope.getBonuse = (memberId) => {
+						let monthlyTask = taskS.monthlyTasks($rootScope.dt.year, $rootScope.dt.month, $rootScope.boardLists, memberId, $rootScope.boardCards);				
+						let monthlyTime = timeS.monthlyTime($rootScope.dt.year, $rootScope.dt.month, $rootScope.boardLists, memberId, $rootScope.boardCards);
+						let bonuse = bonuseS.bonuseTime($rootScope.dt.year, $rootScope.dt.month, $rootScope.boardMembers, $rootScope.boardLists, $rootScope.boardCards, $rootScope.calendarCards);
+						try {
+							if (memberId == bonuse.leader) {
+								return {bonuse:"LEADER", value:10000}
+							}else if (monthlyTask == bonuse.winTask) {
+								return {bonuse:"TASKS", value:5000}
+							}else if (monthlyTime == bonuse.winTime) {
+								return {bonuse:'TIME', value:5000}
+							}else{
+								return {bonuse:'', value:0}
+							}
+						} catch (error) {}
+					};
+                    scope.totalThirteen = (salary, monthDuration, memberId) => {
+						try {
+							let bonuse = scope.getBonuse(memberId)
+							return Math.ceil(((salary * monthDuration)/12) + bonuse.value).toLocaleString() + " PHP"
+						} catch (error) {}
 					}
 				}
 				initialize();
