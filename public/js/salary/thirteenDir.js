fed9ee7c2831ce8107a70cc830de03bcd50afb63
eevan7a9 <e7a9@protonmail.com>
2019-05-03 15:02:28 +0800

    develop thirteenth month pay bonus not yet included

--- public/js/salary/thirteenDir.js
+++ public/js/salary/thirteenDir.js
@@ -1,14 +1,24 @@
 'use strict';
 
 angular.module('workingHoursTrello')
-	.directive('thirteenDir', function ($rootScope) {
+	.directive('thirteenDir', function ($rootScope, totalSalaryS) {
 		return {
 			link : function(scope, element, attrs){
           		function initialize() {
-					scope.menuItem = ['month', 'months', 'basic salary', 'percentage', 'bonus', 'total salary'];
+					scope.menuItem = ['Name', 'months', 'basic salary', 'total'];
 					scope.memberId = "5c32e94ce49690729ecd0794";
-                    
-	
+					scope.getMonthDuration = (memberId) => totalSalaryS.monthDuration($rootScope.calendarLists, $rootScope.calendarCards, 'ENTERING DATE', $rootScope.dt.Date, memberId);
+					// scope.getMonthDuration = (memberId) => totalSalaryS.monthDuration($rootScope.calendarLists, $rootScope.calendarCards, 'ENTERING DATE', new Date('2019/12/7'), memberId);
+					scope.getCurrentSalary = (memberId, monthNumber) => totalSalaryS.salary($rootScope.calendarLists, $rootScope.calendarCards, 'ENTERING DATE', memberId, monthNumber);
+					// scope.getCurrentSalary = (memberId, monthNumber) => totalSalaryS.salary($rootScope.calendarLists, $rootScope.calendarCards, 'ENTERING DATE', memberId, monthNumber);
+					scope.formatSalary = (salary) => {
+						try {
+							return salary.toLocaleString() + " PHP"
+						} catch (error) {}
+					}
+                    scope.totalThirteen = (salary, monthDuration) => {
+						return Math.ceil((salary * monthDuration)/12)
+					}
 				}
 				initialize();
 			},
