12904fb7adf762c1555154d0f0ad3c0c0978a387
eevan7a9 <e7a9@protonmail.com>
2019-05-16 14:31:09 +0800

    fixed wrong month duration

--- public/js/salary/thirteenDir.js
+++ public/js/salary/thirteenDir.js
@@ -7,12 +7,26 @@ angular.module('workingHoursTrello')
           		function initialize() {
 					scope.menuItem = ['Name', 'months', 'basic salary', 'total'];
 					scope.memberId = "5c32e94ce49690729ecd0794";
-					scope.getMonthDuration = (memberId) => totalSalaryS.monthDuration($rootScope.calendarLists, $rootScope.calendarCards, 'ENTERING DATE', $rootScope.dt.Date, memberId);
+					scope.getMonthDuration = (memberId) => {
+						let listId = totalSalaryS.getListByName($rootScope.calendarLists, 'ENTERING DATE');
+						let entryDate = totalSalaryS.getEntryDate($rootScope.calendarCards, listId, memberId);
+						let dateDuration;
+						if (entryDate.getFullYear() == $rootScope.dt.year) {
+							return dateDuration = totalSalaryS.diffInMonths(entryDate, $rootScope.dt.Date);
+						}else{
+							return dateDuration = totalSalaryS.diffInMonths(new Date('2019/01/01'), $rootScope.dt.Date)
+						}
+
+						// return $rootScope.dt.year
+					}
+						// totalSalaryS.monthDuration($rootScope.calendarLists, $rootScope.calendarCards, 'ENTERING DATE', $rootScope.dt.Date, memberId);}
 					// scope.getMonthDuration = (memberId) => totalSalaryS.monthDuration($rootScope.calendarLists, $rootScope.calendarCards, 'ENTERING DATE', new Date('2019/12/7'), memberId);
-					scope.getCurrentSalary = (memberId, monthNumber) => totalSalaryS.salary($rootScope.calendarLists, $rootScope.calendarCards, 'ENTERING DATE', memberId, monthNumber);
+					scope.getCurrentSalary = (memberId) => {
+						let  monthNumber = totalSalaryS.monthDuration($rootScope.calendarLists, $rootScope.calendarCards, 'ENTERING DATE', $rootScope.dt.Date, memberId);
+						return totalSalaryS.salary($rootScope.calendarLists, $rootScope.calendarCards, 'ENTERING DATE', memberId, monthNumber);}
 					scope.formatSalary = (salary) => {
 						try {
-							return salary.toLocaleString() + " PHP"
+							return salary.toLocaleString() + " PHP";
 						} catch (error) {}
 					}
 					scope.getBonuse = (memberId) => {
