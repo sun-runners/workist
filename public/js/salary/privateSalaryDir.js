be4f6ec5e5a1b556d22a2815de19e4f00bf54c8c
eevan7a9 <e7a9@protonmail.com>
2019-05-27 16:40:41 +0800

    statrted developing for private salary

--- public/js/salary/privateSalaryDir.js
+++ public/js/salary/privateSalaryDir.js
@@ -1,14 +1,14 @@
 'use strict';
 
 angular.module('workingHoursTrello')
-	.directive('privateDir', function ($rootScope, privateSalaryS, totalSalaryS, nationalityS) {
+	.directive('privateDir', function ($rootScope, privateSalaryS) {
 		return {
 			link : function(scope, element, attrs){
           		function initialize() {
 					scope.menuItem = ['month', 'months', 'basic salary', 'percentage', 'bonus', 'total salary'];
 					scope.memberId = "5c32e94ce49690729ecd0794";
 					scope.monthWorked = () => {
-						return privateSalaryS.workedMonths($rootScope.calendarLists);
+						return 2323
 					}
 	
 				}
@@ -17,6 +17,6 @@ angular.module('workingHoursTrello')
 			restrict: "EA",
 			replace: true,
 			scope: {},
-			templateUrl: "../../template/salary/directives/privateSalaryDir.html",
+			templateUrl: "../template/salary/directives/privateSalaryDir.html",
 		}
 	});
