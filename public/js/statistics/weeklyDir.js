971a8e7290ee061d13abf8b02fd99af5d10437ac
eevan7a9 <e7a9@protonmail.com>
2019-06-26 14:22:53 +0800

    fixed weekly change to only in weeks statistics

--- public/js/statistics/weeklyDir.js
+++ public/js/statistics/weeklyDir.js
@@ -3,16 +3,34 @@ angular.module('workingHoursTrello')
 		return {
 			link : function(scope, element, attrs){
 				// Initialize Function Section
+				scope.moment = moment();
+
+				// Increase Function Section
+				scope.increaseWeek = function(){
+				  	scope.moment.add(7, 'days');
+				};
+				scope.decreaseWeek = function(){
+					scope.moment.subtract(7, 'days');
+				};
+				scope.getDtOfMoment = function(moment){
+					var dt = {};
+					dt.Date = moment.toDate();
+					dt.year = moment.year();
+					dt.month = moment.month()+1;
+					dt.date = moment.date();
+					dt.week = Math.ceil(moment.date()/7);
+					return dt;
+				}
 				var initialize = function(){
-          		var moment_original = $rootScope.moment.clone();
-					var dt_original = $rootScope.getDtOfMoment(moment_original);
+          		var moment_original = scope.moment.clone();
+					var dt_original = scope.getDtOfMoment(moment_original);
 					scope.dts = [];
 					for(var i=1; i<=7; i++){
 						// Set new moment
 						var moment_in_week = moment_original.clone();
 						moment_in_week = moment_in_week.date((dt_original.week-1)*7+i);
 						// Set new dt
-						var dt_in_week = $rootScope.getDtOfMoment(moment_in_week);
+						var dt_in_week = scope.getDtOfMoment(moment_in_week);
 						// Compare same week
 						if(dt_in_week.week!=dt_original.week) continue;
 						// Push dt
@@ -20,7 +38,8 @@ angular.module('workingHoursTrello')
 					}
 				};
 				// Watch Function Section
-				$rootScope.$watch('moment', function(){
+				scope.$watch('moment', function(){
+					scope.dt = scope.getDtOfMoment(scope.moment);
 					initialize();
 			  	}, true);   
 				scope.getDailyCard = (dateOfDay, memberId) => { /** Get daily card output */
