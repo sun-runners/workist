44e13d687d2b68dee7d9895ab3e1cb6de64bd3d9
eevan7a9 <e7a9@protonmail.com>
2020-05-18 07:43:00 +0900

    fixed getYearlyToWork

--- public/js/statistics/yearlyDir.js
+++ public/js/statistics/yearlyDir.js
@@ -1,12 +1,12 @@
 'use strict';
 
 angular.module('workingHoursTrello')
-	.directive('yearlyDir', function ($rootScope, monthS, yearS, holidayS, totalSalaryS, birthdayS) {
+	.directive('yearlyDir', function ($rootScope, monthS) {
 		return {
 			link : function(scope, element, attrs){
 
 				// Initialize Function Section
-				var initialize = function(){
+				let initialize = function(){
 					let moment_original = $rootScope.moment.clone();
 					scope.thisDate = moment_original;
 					scope.months = [
@@ -38,31 +38,18 @@ angular.module('workingHoursTrello')
 							return holiday.country.toLowerCase() == member.nationality.toLowerCase() && holiday.year == $rootScope.dt.year;
 						}
 					});
-					return monthS.monthNeedToWork(member, monthWeeksDates, foundCurrentHolidays);
+					return monthS.datesNeedToWork(member, monthWeeksDates, foundCurrentHolidays);
 				}
 
-				scope.getMonthlyToWork = (memberId, month, nationality, entry) => {
-					const entered_date = new Date(entry);
-					const started_full_date =  `${entered_date.getFullYear()}/${entered_date.getMonth()+1}/${entered_date.getDate()}`;
-					const end_date = new Date($rootScope.dt.year, month, 0).getDate();
-					const end_full_date = `${$rootScope.dt.year}/${month}/${end_date}`;
-
-					let toWork;
-					if (entered_date.getFullYear() == $rootScope.dt.year && entered_date.getMonth()+1 == month) {
-						const month_to_work = monthS.monthsNeedtoWork(scope.thisDate.year(), month);
-						toWork = monthS.getInBetweenDates(month_to_work, started_full_date, end_full_date) 
-
-					}else{
-						toWork = monthS.monthsNeedtoWork(scope.thisDate.year(), month); /** get the total days to work whith holiday not a factor  */
-					}
-					let filterPrevBirthday = birthdayS.removeBirthdate(memberId, $rootScope.calendarLists, $rootScope.calendarCards, "BIRTHDAY", toWork);
-					return holidayS.datesWithoutHoliday(nationality, $rootScope.dt.year, $rootScope.calendarLists, $rootScope.calendarCards, filterPrevBirthday);
+				scope.getYearlyToWork = (member) => {
+					let yearsMonthDates = scope.months.map(month => month.dates).flat(Infinity);
+					const foundCurrentHolidays = $rootScope.holidays.find(holiday => {
+						if (member.nationality) {
+							return holiday.country.toLowerCase() == member.nationality.toLowerCase() && holiday.year == $rootScope.dt.year;
+						}
+					});
+					return monthS.datesNeedToWork(member, yearsMonthDates, foundCurrentHolidays);
 				}
-				scope.getYearlyToWork = (nationality) => {
-					let toWork = yearS.yearsNeedToWork($rootScope.dt.year, scope.months); /** get the total days to work whith holiday not a factor  */
-					return holidayS.datesWithoutHoliday(nationality, $rootScope.dt.year, $rootScope.calendarLists, $rootScope.calendarCards, toWork);
-				}
-				scope.showMonthly = (work, toWork) => (work != 0) ? work+' / '+toWork : '-';
 			},
 			restrict: "EA",
 			replace: true,
